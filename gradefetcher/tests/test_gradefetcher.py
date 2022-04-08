74f65baf70034d97990c0264fde1677ebcd4180d
Anders Pearson <anders@thraxil.org>
2022-04-08 09:51:25 +0100

    add test for is_valid_url()

--- gradefetcher/tests/test_gradefetcher.py
+++ gradefetcher/tests/test_gradefetcher.py
@@ -1,6 +1,6 @@
 import unittest
 
-from gradefetcher.gradefetcher import grade_from_list
+from gradefetcher.gradefetcher import GradeFetcherXBlock, grade_from_list
 
 
 class GradeFetcherHelperTests(unittest.TestCase):
@@ -11,3 +11,10 @@ class GradeFetcherHelperTests(unittest.TestCase):
         assert grade_from_list([0, 0]) == 0
         assert grade_from_list([1, 1]) == 100
         assert grade_from_list([1, 0]) == 50
+
+    def test_is_valid_url(self):
+        block = GradeFetcherXBlock(runtime=None, scope_ids=None)
+        assert block.is_valid_url("https://www.example.com/")
+        assert block.is_valid_url("http://www.example.com/")
+        assert not block.is_valid_url("htt://www.example.com/")
+        assert not block.is_valid_url("https://example/")
