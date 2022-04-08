f5442867f58f3b09dd96b744cc63ccd802780617
Anders Pearson <anders@thraxil.org>
2022-04-08 10:21:12 +0100

    remove dummy test
    no longer needed (to make tox pass) now that we have some actual tests
    to run.

--- gradefetcher/tests/test_dummy.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-class GradeFetcherXBlockTests(unittest.TestCase):
-    def test_dummy(self):
-        # just a test to check that the test suite is running
-        assert 2 + 2 == 4
