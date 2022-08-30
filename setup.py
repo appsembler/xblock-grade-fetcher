60946ade689dffc02d86a0f162d9e1ba8359f398
Amir Tadrisi <amirtds@gmail.com>
2022-08-30 14:46:22 -0500

    chore: bump version to 0.4.7

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-grade-fetcher',
-    version='0.4.6',
+    version='0.4.7',
     description='Grade Fetcher',
     license='AGPL v3',
     packages=[
